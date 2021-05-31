bf2083a0669c8002b84d1aa30debed41d4facd6b
seongryeol-han <root@DESKTOP-JRHM3NP.localdomain>
2021-05-31 21:53:30 +0900

    requirements

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "e+90ujsym-qszs7b7@hij%c^#fvm_cp4sw2zl!hfoz*jfrcvy9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  #
+DEBUG = bool(os.environ.get("DEBUG"))  #
 
 ALLOWED_HOSTS = "*"
 
@@ -94,13 +94,21 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
+if DEBUG:
 
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
