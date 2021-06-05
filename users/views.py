4c8aa25299eccdb16f723bf9d8932d78a0a3e5bc
seongryeol-han <root@DESKTOP-JRHM3NP.localdomain>
2021-06-05 15:19:21 +0900

    login

--- users/views.py
+++ users/views.py
@@ -79,7 +79,7 @@ def complete_verification(request, key):
 
 def github_login(request):  # user를 github으로 보내고
     client_id = os.environ.get("GH_ID")
-    redirect_uri = "http://127.0.0.1:8000/users/login/github/callback"
+    redirect_uri = "http://airbnb-clone.eba-zp26tvmm.ap-northeast-2.elasticbeanstalk.com/users/login/github/callback"
     return redirect(
         f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user"
     )
@@ -155,7 +155,7 @@ def github_callback(request):  # github에서 user를 우리 app으로 가져온
 
 def kakao_login(request):  # user를 github으로 보내고
     client_id = os.environ.get("KAKAO_ID")  # REST API Key
-    redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"  # 다시 되받는곳.
+    redirect_uri = "http://airbnb-clone.eba-zp26tvmm.ap-northeast-2.elasticbeanstalk.com/users/login/kakao/callback"  # 다시 되받는곳.
     return redirect(
         f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
     )  # 여기로 오는거다.
@@ -169,7 +169,7 @@ def kakao_callback(request):
     try:
         code = request.GET.get("code")
         client_id = os.environ.get("KAKAO_ID")
-        redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
+        redirect_uri = "http://airbnb-clone.eba-zp26tvmm.ap-northeast-2.elasticbeanstalk.com/users/login/kakao/callback"
         token_request = requests.get(
             f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}"
         )
